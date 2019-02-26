1f5463683bbfffb71f672f90ca94626a2d7d06da
einomies26 <einokamarainen@hotmail.com>
2019-02-26 23:01:37 +0200

    react-table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import VirtualList from 'react-tiny-virtual-list';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
 
 class App extends Component {
 
@@ -25,21 +26,37 @@ class App extends Component {
   }
 
   render() {
-    const tableRows = this.state.data.map((item, index) =>
-      <tr key={index}>
-        <td>{item.full_name}</td>
-        <td><a href={item.html_url}>{item.html_url}</a></td>
-      </tr>
-    )
+
+    // We will define our columns by creating the array of column objects into
+    // the render() method. In a column object, you have to define at least the header
+    // of the column and the data accessor. The data accessor values come from our
+    // REST API response data. You can see that our response data contains an object
+    // called owner, and we can show these values using the owner.field_name
+    // syntax:
+    const columns = [
+      {
+        Header: 'Name', // Header of the column
+        accessor: 'full_name' // Value accessor
+      },
+      {
+        Header: 'URL',
+        accessor: 'html_url'
+      },
+      {
+        Header: 'Owner',
+        accessor: 'owner.login'
+      }
+    ]
+
     return (
       <div className="App">
         <input type="text" onChange={this.handleChange} />
         <button onClick={this.fetchData} value={this.state.keyword}>Fetch</button>
-        <table>
-          <tbody>
-            {tableRows}
-          </tbody>
-        </table>
+
+        {/* To fill React Table with data, you have to pass the data prop to the component.
+        Data can be an array or object and therefore we can use our state, called data.
+        Columns are defined using the columns prop and that prop is required: */}
+        <ReactTable data={this.state.data} columns={columns} />
       </div>
     );
   }
